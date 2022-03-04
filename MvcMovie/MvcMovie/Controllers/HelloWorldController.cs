890639c076a1d63ff0b4fd556aafe2bad7c542c6
franciscozapata12346 <franciscozapata12346@gmail.com>
2022-03-04 16:57:43 -0300

    avance

--- MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -16,11 +16,19 @@ namespace MvcMovie.Controllers
             return View();
         }
 
-        // GET: /HelloWorld/Welcome/ 
-        // Requires using System.Text.Encodings.Web;
-        public string Welcome(string name, int ID = 1)
+        //// GET: /HelloWorld/Welcome/ 
+        //// Requires using System.Text.Encodings.Web;
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        //}
+
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
