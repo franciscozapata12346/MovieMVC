71876399dcddedeb63cc79d73eb4e20884f9bf31
franciscozapata12346 <franciscozapata12346@gmail.com>
2022-03-25 18:41:51 -0300

    Avances

--- MvcMovie/MvcMovie/Models/Movie.cs
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -18,8 +18,7 @@ namespace MvcMovie.Models
         public DateTime ReleaseDate { get; set; }
 
         public string Genre { get; set; }
-
-        [Column(TypeName = "decimal(18, 2)")]
+        
         public decimal Price { get; set; }
     }
 }
